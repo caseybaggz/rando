4922df65dee23428b9801d9631d0a4604c33adfd
Casey Baggz <caseybaggz@gmail.com>
2023-08-27 13:19:44 -0500

    add panda

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,10 +1,10 @@
 import { createSignal } from "solid-js";
-import "./Counter.css";
+import { button } from "~/recipes/button";
 
 export default function Counter() {
   const [count, setCount] = createSignal(0);
   return (
-    <button class="increment" onClick={() => setCount(count() + 1)}>
+    <button class={button()} onClick={() => setCount(count() + 1)}>
       Clicks: {count()}
     </button>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,12 @@
 import { Title } from "solid-start";
+import { container } from "~/../styled-system/patterns";
 import Counter from "~/components/Counter";
 
 export default function Home() {
   return (
-    <main>
+    <main class={container({ py: '10' })}>
       <Title>Hello World</Title>
-      <h1>Hello world!</h1>
       <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
     </main>
   );
 }
