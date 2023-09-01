aff16dac2b9916d908ca94aacdd6ed9d71ac1142
Casey Baggz <caseybaggz@gmail.com>
2023-09-01 08:24:19 -0500

    add link example

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
-import { Title } from "solid-start";
+import { A, Title } from "solid-start";
+import { css } from "~/../styled-system/css";
 import { container } from "~/../styled-system/patterns";
 import Counter from "~/components/Counter";
 
@@ -7,6 +8,8 @@ export default function Home() {
     <main class={container({ py: '10' })}>
       <Title>Hello World</Title>
       <Counter />
+
+      <A href="/" class={css({ display: 'inline-flex', p: '4', bg: 'blue.500', color: 'white', borderRadius: 'md' })}>Home</A>
     </main>
   );
 }
